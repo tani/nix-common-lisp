8b0859a7d9b588b0407c8bcf78a1e951943f225d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-30 20:16:47 +0900

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
-OLDNAME="fibonacci_new"
+OLDNAME="fibonacci"
 NEWNAME="fibonacci"
 find . -type f -not -name "rename.sh" -exec rename "$OLDNAME" "$NEWNAME" {} \;
 find . -type f -exec rename "$OLDNAME" "$NEWNAME" {} \;
