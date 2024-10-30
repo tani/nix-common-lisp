2d334d45d773344a652b717a261c8cf0e36702d2
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-30 20:05:40 +0900

    format code

--- flake.nix
+++ flake.nix
@@ -9,44 +9,43 @@
     self,
     nixpkgs,
     flake-parts,
-    systems
+    systems,
   }:
-  flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = import systems;
-    perSystem = { pkgs, ... }:
-    let
-      pname = "fibonacci";
-      version = "0.0.0";
-      lispLib  = pkgs.sbcl.buildASDFSystem {
-        inherit pname version;
-        src = ./.;
-        systems = [pname "${pname}/test"];
-        lispLibs = with pkgs.sbcl.pkgs; [
-          parachute
-        ];
-      };
-      lispApp = pkgs.sbcl.withPackages (ps: [ lispLib ]);
-      main = pkgs.writeShellScriptBin "${pname}-main" ''
-        ${lispApp}/bin/sbcl --noinform --non-interactive --eval "(require :asdf)" --eval "(asdf:make :${pname})" --eval "(${pname}:main)"
-      '';
-      test = pkgs.writeShellScriptBin "${pname}-test" ''
-        ${lispApp}/bin/sbcl --noinform --non-interactive --eval "(require :asdf)" --eval "(asdf:test-system :${pname})"
-       '';
-    in {
-      packages.default = lispLib;
-      devShells.default = pkgs.mkShell {
-        packages = [ lispApp ];
-      };
-      apps = {
-        default = {
-          type = "app";
-          program = main;
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import systems;
+      perSystem = {pkgs, ...}: let
+        pname = "fibonacci";
+        version = "0.0.0";
+        lispLib = pkgs.sbcl.buildASDFSystem {
+          inherit pname version;
+          src = ./.;
+          systems = [pname "${pname}/test"];
+          lispLibs = with pkgs.sbcl.pkgs; [
+            parachute
+          ];
         };
-        test = {
-          type = "app";
-          program = test;
+        lispApp = pkgs.sbcl.withPackages (ps: [lispLib]);
+        main = pkgs.writeShellScriptBin "${pname}-main" ''
+          ${lispApp}/bin/sbcl --noinform --non-interactive --eval "(require :asdf)" --eval "(asdf:make :${pname})" --eval "(${pname}:main)"
+        '';
+        test = pkgs.writeShellScriptBin "${pname}-test" ''
+          ${lispApp}/bin/sbcl --noinform --non-interactive --eval "(require :asdf)" --eval "(asdf:test-system :${pname})"
+        '';
+      in {
+        packages.default = lispLib;
+        devShells.default = pkgs.mkShell {
+          packages = [lispApp];
+        };
+        apps = {
+          default = {
+            type = "app";
+            program = main;
+          };
+          test = {
+            type = "app";
+            program = test;
+          };
         };
       };
     };
-  };
 }
