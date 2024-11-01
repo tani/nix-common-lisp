dade08b33846316c9eefda953430d5072676b87a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-11-01 21:48:32 +0900

    fix(rename.sh): replace rename with sed for content change
    The script now uses `sed` to replace occurrences of the old name with the
    new name within file contents, instead of using `rename`. This ensures
    that the content of the files is updated correctly. The `rename` command
    is still used to rename the files themselves.

--- rename.sh
+++ rename.sh
@@ -2,5 +2,5 @@
 set -eu -o pipefail
 OLDNAME="fibonacci"
 NEWNAME="fibonacci"
-find . -type f -not -name "rename.sh" -exec rename "$OLDNAME" "$NEWNAME" {} \;
+find . -type f -not -name "rename.sh" -exec sed -i "s/$OLDNAME/$NEWNAME/g" {} \;
 find . -type f -exec rename "$OLDNAME" "$NEWNAME" {} \;
